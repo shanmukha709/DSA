f26b6b3cdcd185c7b7ce13cd8009461dfc2741d9
Shanmukha Kadumuru <shanmukhak709@gmail.com>
2025-07-29 11:54:24 +0530

    New codes

---
+++ src/main/java/com/dsa/practiceDSA/arrays/MajorityElement.java
@@ -0,0 +1,22 @@
+package com.dsa.practiceDSA.arrays;
+
+public class MajorityElement{
+
+    static int majorityelement(int[] arr){
+        int count = 0;
+        int candidate = 0;
+
+        for(int num : arr){
+            if(count == 0){
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,2,1,1,1};
+        System.out.println(majorityelement(arr));
+    }
+}
